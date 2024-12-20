6d4499d524b83efed2d8acd6e74376af09742738
NKWDD <kamilkala125@gmail.com>
2024-12-21 00:31:49 +0100

    Updated nextauth.js with changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,7 @@ import GoogleProvider from 'next-auth/providers/google'
 const adminEmails = ['kamilkala125@gmail.com']
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
