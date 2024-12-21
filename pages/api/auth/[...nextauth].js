7a42906b9ca84c552f4c41b78837f2038ba355ee
NKWDD <kamilkala125@gmail.com>
2024-12-21 16:31:33 +0100

    Fix 2fa

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,33 +1,29 @@
-import NextAuth, { getServerSession } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import clientPromise from '@/lib/mongodb';
-import { Admin } from '@/models/Admin';
-import { mongooseConnect } from '@/lib/mongoose';
+import NextAuth, {getServerSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import {Admin} from "@/models/Admin";
+import {mongooseConnect} from '@/lib/mongoose';
 
-// Helper function to check if an email belongs to an admin
 async function isAdminEmail(email) {
-  await mongooseConnect(); // Ensure database connection
-  return !!(await Admin.findOne({ email }));
+  mongooseConnect();
+  return !! (await Admin.findOne({email}));
 }
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: async ({ session }) => {
-      // Check if the user's email belongs to an admin
-      const isAdmin = await isAdminEmail(session?.user?.email);
-      if (isAdmin) {
-        session.user.isAdmin = true; // Add isAdmin flag to session
+    session: async ({session,token,user}) => {
+      if (await isAdminEmail(session?.user?.email)) {
         return session;
       } else {
-        return null; // Return null to deny session creation for non-admin users
+        return false;
       }
     },
   },
@@ -35,11 +31,11 @@ export const authOptions = {
 
 export default NextAuth(authOptions);
 
-// Middleware function to restrict access to admin routes
-export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!session || !session.user?.isAdmin) {
-    res.status(401).json({ message: 'Unauthorized. Only admins can access this route.' });
-    throw new Error('Not an admin');
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!(await isAdminEmail(session?.user?.email))) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
   }
 }
